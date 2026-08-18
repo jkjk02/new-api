6ed6d1e9083fe6a607bc52c5b61ba7ec17fbc65d
沈从文 <scongwen14@gmail.com>
2026-08-18 05:30:27 -1000

    feat(payload-log): update router/api-router.go

--- router/api-router.go
+++ router/api-router.go
@@ -278,6 +278,10 @@ func SetApiRouter(router *gin.Engine) {
 		logRoute.GET("/self", middleware.UserAuth(), controller.GetUserLogs)
 		logRoute.GET("/self/search", middleware.UserAuth(), middleware.SearchRateLimit(), controller.SearchUserLogs)
 
+		payloadLogRoute := apiRouter.Group("/payload_log")
+		payloadLogRoute.GET("/", middleware.AdminAuth(), controller.GetPayloadLogs)
+		payloadLogRoute.GET("/:id", middleware.AdminAuth(), controller.GetPayloadLogDetail)
+
 		systemTaskRoute := apiRouter.Group("/system-task")
 		systemTaskRoute.Use(middleware.RootAuth())
 		{
